2dec7a9b09ede698ff52112f8e029027ce057e4e
Thiago <thiagolima1011@gmail.com>
2021-06-28 13:57:13 -0300

    Adicao dao jogador

--- src/com/confluence/dao/JogadorDao.java
+++ src/com/confluence/dao/JogadorDao.java
@@ -18,7 +18,76 @@ public class JogadorDao {
 		this.connection = new ConnectionFactory().getConnection();
 	}
 
-	
+	public void adiciona(Jogador jogador) {
+		String sql = "insert into selecao (idclube, nome, idade) values (?,?,?)";
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setInt(1, jogador.getClube());
+			stmt.setString(2, jogador.getNome());
+			stmt.setInt(3, jogador.getIdade());
+			stmt.executeUpdate();
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex);
+		} // fim try }//fim adiciona
+	}
+
+	public List<Jogador> getLista() {
+		String sql = "select * from jogador";
+		List<Jogador> jogadores = new ArrayList();
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Jogador jogador = new Jogador();
+				jogador.setClube(rs.getInt("idclube"));
+				jogador.setNome(rs.getString("nome"));
+				jogador.setIdade(rs.getInt("idade"));
+				jogadores.add(jogador);
+			} // fim while
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex);
+		} // fim try
+		return jogadores;
+	}// fim lista
+
+	public Jogador busca(Integer idjogador) {
+		String sql = "select * from jogador where idjogador = ?";
+		Jogador jogador = null;
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setInt(1, idjogador);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				jogador = new Jogador();
+				jogador.setIdJogador(rs.getInt("idjogador"));
+				jogador.setClube(rs.getInt("idclube"));
+				jogador.setNome(rs.getString("nome"));
+				jogador.setIdade(rs.getInt("idade"));
+			} // fim if
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex);
+		} // fim try
+		return jogador;
+	}// fim busca
+
+	public void altera(Jogador jogador) {
+		String sql = "update selecao set idclube = ?, nome = ?, idade = ? where idjogador = ?";
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setString(1, jogador.getNome());
+			stmt.setInt(2,  jogador.getClube());
+			stmt.setInt(3, jogador.getIdade());
+			stmt.executeUpdate();
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex);
+		} // fim try }//fim altera
+	}
+
+	public void remove(Jogador jogador) { 
+		String sql = "delete from jogador where idjogador = ?"; 
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) { 
+			stmt.setLong(1, jogador.getIdJogador()); 
+			stmt.executeUpdate(); 
+		} catch (SQLException ex) { 
+			throw new RuntimeException(ex); 
+		}//fim try 
+	} //fim remove
 }
 	
 
